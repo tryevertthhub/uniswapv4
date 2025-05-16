a6113f217a926db42a59053e58f0a9d7e4c855a2
tryevertthhub <tryevertth@gmail.com>
2025-05-16 00:44:11 -0700

    feat: add dependencies

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Numerics;
+using System.Threading.Tasks;
+using Nethereum.Web3;
+using Nethereum.Util;
+using Nethereum.ABI;
+using Nethereum.ABI.FunctionEncoding;
+using Nethereum.Uniswap.V4.PositionManager;
+using Nethereum.Uniswap.V4.StateView;
+using Nethereum.Uniswap.V4.StateView.ContractDefinition;
+using Nethereum.Uniswap.V4.V4Quoter.ContractDefinition;
+using Nethereum.Hex.HexConvertors.Extensions; 
+using System.Net.Http;
+using System.Text.Json;
+using Nethereum.JsonRpc.Client;
+using System.Reflection.Metadata.Ecma335;
+using Nethereum.ABI.Encoders;
+using Nethereum.ABI.Model;
+using System.Drawing;
+using System;
+using System.Linq;
+using System.Text;
+using Nethereum.Hex.HexTypes;
+using Nethereum.StandardTokenEIP20;
+using PeterO.Numbers;
+using MathNet.Symbolics;
+using Expr = MathNet.Symbolics.SymbolicExpression;
+
+using static Program;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        
+    }
+}
